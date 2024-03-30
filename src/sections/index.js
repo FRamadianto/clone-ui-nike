d28d9dc5040db3874482d55a2394ed63f3e53a66
Fikri Ramadianto <ramadiantofikri@gmail.com>
2024-03-30 16:26:50 +0700

    first commit implement component structure

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularPrducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  CustomerReviews,
+  Hero,
+  Footer,
+  PopularPrducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
