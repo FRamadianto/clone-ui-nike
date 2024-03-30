a30f1f95c3db9b97fb550c1bc216227f877b9a6a
Fikri Ramadianto <ramadiantofikri@gmail.com>
2024-03-30 23:59:12 +0700

    hero-section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,53 @@
+import Button from "../components/Button";
+import arrowRight from "../assets/icons/arrow-right.svg";
+import { statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-coral-red text-xl font-montserrat ">
+          Our Summer Collections
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red mt-3 mr-4">Nike</span>
+          Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals quality comfort, and innovation for
+          your active life.{" "}
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((item) => (
+            <div key={item.label}>
+              <p className="text-4xl font-palanquin font-bold">{item.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {item.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={600}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
